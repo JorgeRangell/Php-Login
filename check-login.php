ab7181605528b7487fdc1b6d49284ccede36add9
Jorge Rangel <101427212+JorgeRangell@users.noreply.github.com>
2023-11-04 23:44:30 -0300

    Delete check-login.php

--- check-login.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    $usuario = $_SESSION['usuario'];
-    if(!isset($_SESSION['usuario'])){
-        header('location:index.php?erro_login=true');
-        exit;   
-    }
-?>
