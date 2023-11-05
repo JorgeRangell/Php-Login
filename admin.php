157fa4bf34174302c603ac5f11e17d9fe504bac8
Jorge Rangel <101427212+JorgeRangell@users.noreply.github.com>
2023-11-04 23:46:12 -0300

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,27 @@
+<?php
+    include'check-login.php';
+
+    if($_SESSION['nivel'] != "A"){
+        session_destroy();
+        header('location:index.php?erro_login=true');
+        exit;
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-bt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="css/style-login.css">
+</head>
+<body>
+<h1>Pagina de Administração</h1>
+    <p>usuario logado:
+        <?php echo $usuario; ?>
+    </p>
+
+    <a href="logout.php">Sair</a>   
+</body>
+</html>
